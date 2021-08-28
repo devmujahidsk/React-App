27f17e83367a2c6d69d1338f2c7c00c176d293a7
Makemelive <support@makemelive.in>
2021-08-28 19:44:28 +0530

    menu changes done

--- src/components/navigation/menu-item.js
+++ src/components/navigation/menu-item.js
@@ -15,10 +15,36 @@ export default function MenuItem() {
                     </div>
                     <nav className={hamburger ? 'main-menu menu-open' : 'main-menu menu-close'}>
                         <ul className="navbar-wrap" onClick={showHamburger}>
-                            <li><A href="/"><span className="pos">01.</span><div classNam="list-item">Home</div></A></li>
-                            <li><A href="/about"><span className="pos">02.</span><div classNam="list-item">About</div></A></li>
-                            <li><A href="/our-stories"><span className="pos">03.</span><div classNam="list-item">Our Stories</div></A></li>
-                            <li><A href="/contact"><span className="pos">04.</span><div classNam="list-item">Contact</div></A></li>
+                            <li>
+                                <A href="/">
+                                    <span className="pos">01.</span>
+                                    <div classNam="list-item">Home</div>
+                                </A>
+                            </li>
+                            <li>
+                                <A href="/about">
+                                    <span className="pos">02.</span>
+                                    <div classNam="list-item">About</div>
+                                </A>
+                            </li>
+                            <li>
+                                <A href="/our-stories">
+                                    <span className="pos">03.</span>
+                                    <div classNam="list-item">Our Stories</div>
+                                </A>
+                            </li>
+                            <li>
+                                <A href="/contact">
+                                    <span className="pos">04.</span>
+                                    <div classNam="list-item">Contact</div>
+                                </A>
+                            </li>
+                            <li>
+                                <A href="/registration">
+                                    <span className="pos">05.</span>
+                                    <div classNam="list-item">Registration</div>
+                                </A>
+                            </li>
                         </ul>
                     </nav>
                     <div className="hamburger-wrap" onClick={showHamburger}>
